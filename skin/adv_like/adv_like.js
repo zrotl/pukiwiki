e20bda83f901b61d20eb0b0f687a755bcbf6ec58
zrotl <89401398+zrotl@users.noreply.github.com>
2023-03-22 00:11:05 +0900

    refactoring Skin JavaScript

--- skin/adv_like/adv_like.js
+++ skin/adv_like/adv_like.js
@@ -1,36 +1,41 @@
-//html elements Position Adjustment
+// html elements
 const logo = document.getElementById("header-logo");
-logo.style.width = logo.getBoundingClientRect().height+"px";
-
 const spheader = document.getElementById("sp-header");
 const spnavi = document.getElementById("sp-navigator");
 const contents = document.getElementById("contents");
-spnavi.style.marginTop = "-"+spnavi.getBoundingClientRect().height+"px";
-spnavi.style.top = (spheader.getBoundingClientRect().height-spnavi.getBoundingClientRect().height)+"px";
+const menubar = document.getElementById("menubar");
+const checkbox = document.getElementById("color_mode_switch");
+const getCookie = Cookies.get('pkwk-darkmode');
 
+// initialize Positions
+calcLogoSize();
+calcContentsHeight();
+changeStyleSheet((typeof getCookie === 'undefined') ? 
+    window.matchMedia('(prefers-color-scheme: dark)').matches
+    : JSON.parse(Cookies.get('pkwk-darkmode'))
+);
+
+
+// display mode change(sp -> pc)
 const mql = window.matchMedia('(max-width: 767px)');
 mql.onchange = (e) => {
-    if (!e.matches) {
-        logo.style.width = logo.getBoundingClientRect().height+"px";
-        contents.style.position = 'static';
-    }
+    if (!e.matches) calcLogoSize();
 }
 
+
+// Window Size Event Listener(sp)
 window.addEventListener('resize',function(){
-    spnavi.style.marginTop = "-"+spnavi.getBoundingClientRect().height+"px";
-    spnavi.style.top = (spheader.getBoundingClientRect().height-spnavi.getBoundingClientRect().height)+"px";
-    contents.style.top = spheader.getBoundingClientRect().height+"px";
+    calcContentsHeight();
 });
 
-
-//CSS selector switch
-const checkbox = document.getElementById("color_mode_switch");
-
+// CSS selector switch
 checkbox.addEventListener('change', function () {
     changeStyleSheet(checkbox.checked);
     Cookies.set('pkwk-darkmode', (checkbox.checked ? 1 : 0), {expires: 180});
 });
 
+
+// define functions
 function changeStyleSheet(mode) {
     const element = document.getElementById("colorstyle");
 
@@ -43,11 +48,13 @@ function changeStyleSheet(mode) {
     }
 }
 
-const getCookie = Cookies.get('pkwk-darkmode');
-let dark_mode;
-if (typeof getCookie === 'undefined') {
-    dark_mode = window.matchMedia('(prefers-color-scheme: dark)').matches;
-} else {
-    dark_mode = JSON.parse(Cookies.get('pkwk-darkmode'));
+function calcContentsHeight() {
+    spnavi.style.marginTop = "-"+spnavi.getBoundingClientRect().height+"px";
+    spnavi.style.top = (spheader.getBoundingClientRect().height-spnavi.getBoundingClientRect().height)+"px";
+    contents.style.top = spheader.getBoundingClientRect().height+"px";
+    menubar.style.top = spheader.getBoundingClientRect().height+"px";
 }
-changeStyleSheet(dark_mode);
+
+function calcLogoSize() {
+    logo.style.width = logo.getBoundingClientRect().height+"px";
+}
