06df125e06cc9130a891cbf4134c0f80e659132f
zrotl <89401398+zrotl@users.noreply.github.com>
2023-03-27 23:56:28 +0900

    Edit Mod Version (230327)

--- lib/init.php
+++ lib/init.php
@@ -11,7 +11,7 @@
 // PukiWiki version / Copyright / Licence
 
 define('S_VERSION', '1.5.4');
-define('S_MOD_VERSION', '230319');
+define('S_MOD_VERSION', '230327');
 define('S_COPYRIGHT',
 	'<strong>PukiWiki '
 	. S_VERSION
