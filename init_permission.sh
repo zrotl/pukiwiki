6c08dcef8e8da85e298b704c61ae4cbfadb3575a
zrotl <89401398+zrotl@users.noreply.github.com>
2023-04-25 00:08:57 +0900

    fix permissions

--- init_permission.sh
+++ init_permission.sh
@@ -2,6 +2,6 @@
 
 USERNAME=$(whoami)
 chown -R $USERNAME.$USERNAME *
-chmod 777 attach backup cache counter diff log log/update htmlinsert wiki wiki.en
+chmod 777 attach backup cache counter diff log log/update wiki wiki.en
 chmod 755 image image/face lib plugin skin
 chmod 666 cache/*.dat cache/*.ref cache/*.rel skin/*.js wiki/*.txt wiki.en/*.txt
