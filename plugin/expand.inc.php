ac35e7c833551705de831ed63d252d174e83a963
zrotl <89401398+zrotl@users.noreply.github.com>
2023-05-09 13:23:06 +0900

    add expand plugin

---
+++ plugin/expand.inc.php
@@ -0,0 +1,69 @@
+<?php
+/**
+* detailsタグを使ったシンプルな折りたたみプラグイン
+*
+* @version 1.1.1
+* @author kanateko, modified by root
+* @link https://jpngamerswiki.com/?56478a40a9
+* @license https://www.gnu.org/licenses/gpl-3.0.html GPLv3
+* -- Updates --
+* 2023-05-09 v1.1.1 detail部分のpタグを消去
+* 2022-06-17 v1.1 サマリ部分にPukiWiki記法を使用できるよう拡張
+* 2020-02-10 v1.0 コードの微調整&整理
+* 2019-09-22 v0.8 初版作成
+*/
+
+function plugin_expand_init()
+{
+    $msg['_expand_messages'] = [
+        'label_summary' => '詳細を表示'
+    ];
+    set_plugin_messages($msg);
+}
+
+function plugin_expand_convert(...$args)
+{
+    global $_expand_messages;
+
+    if (count($args) < 1) return;
+
+    $details = array_pop($args);
+    $details = preg_replace("/\r|\r\n/", "\n", $details);
+
+    // オプション振り分け
+    foreach ($args as $arg) {
+        if (preg_match('/^open$/',$arg)) {
+            // 展開して表示
+            $tag = ' open';
+        } elseif (preg_match('/^(color|size)=(.+)$/',$arg, $m)) {
+            // summaryのスタイル
+            $options[$m[1]] = htmlsc($m[2]);
+        } else {
+            // その他はsummaryとする
+            $summary = make_link($arg);
+        }
+    }
+
+    // 表示内容の最終調整
+    $tag = $tag ?: '';
+    $summary = $summary ?: $_expand_messages['label_summary'];
+    $size = isset($options['size']) ? ' style="font-size:' . $options['size'] . ';"' : '';
+    if ($options['color']) {
+        $summary = '<span style="color:' . $options['color'] . ';">' . $summary . '</span>';
+    }
+    $details = convert_html($details);
+
+    //pタグの消去
+    $details = str_replace('<p>', '', $details);
+    $details = str_replace('</p>', '', $details);
+
+    // 実際に表示する内容
+    $body = <<<EOD
+    <details class="plugin-expand"$tag>
+        <summary$size>$summary</summary>
+        $details
+    </details>
+    EOD;
+
+    return $body;
+}
